bdaae24417b44c556c0fb29c875c0360705ff9fe
Achmad Muchlasin <ahmad.mukhlas61@gmail.com>
2023-01-29 16:35:32 +0700

    update function search

--- lib/view/pages/home_screen.dart
+++ lib/view/pages/home_screen.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 // ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:sikomo/material_assets/model/sikomo.dart';
 import 'package:sikomo/view/pages/tour_details_screen.dart';
 import '../../material_assets/font/typography.dart';
 import '../../material_assets/widget/product_wisata.dart';
@@ -18,149 +19,186 @@ class _HomeScreenState extends State<HomeScreen> {
   TextEditingController search = TextEditingController();
 
   // data list api
-  List dataSikomo = [];
+  List<Sikomo> dataSikomo = [];
+
+  // list untuk display
+  late List<Sikomo> displaySikomo = List.from(dataSikomo);
+
   // loading circular
   bool isLoading = true;
 
-  Future getData() async {
-    try {
-      final response =
-          await http.get(Uri.parse('http://192.168.218.183/SIKOMO/read.php'));
+  // controller search
+  TextEditingController searchData = TextEditingController();
 
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        setState(() {
-          dataSikomo = data;
-          isLoading = false;
-        });
-      }
-    } catch (e) {
-      print(e);
+  Future getDataSikomo() async {
+    final response =
+        await http.get(Uri.parse('http://192.168.1.87/SIKOMO/read.php'));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body) as List<dynamic>;
+      final transfrom = data.map((e) {
+        return Sikomo(
+            id: e['id'],
+            nama: e['nama'],
+            deskripsi: e['deskripsi'],
+            rating: e['rating'],
+            lokasi: e['lokasi'],
+            jam: e['jam'],
+            harga: e['harga'],
+            fasilitas_1: e['fasilitas_1'],
+            fasilitas_2: e['fasilitas_2'],
+            image: e['image']);
+      }).toList();
+      setState(() {
+        dataSikomo = transfrom;
+        isLoading = false;
+      });
+      print('fetch data berhasil');
     }
   }
 
+  void searchApi(String data) {
+    setState(() {
+      displaySikomo = dataSikomo
+          .where(
+              (value) => value.nama.toLowerCase().contains(data.toLowerCase()))
+          .toList();
+    });
+  }
+
+  // Future getData() async {
+  //   try {
+  //     final response =
+  //         await http.get(Uri.parse('http://192.168.1.87/SIKOMO/read.php'));
+
+  //     if (response.statusCode == 200) {
+  //       final data = jsonDecode(response.body);
+  //       setState(() {
+  //         dataSikomo = data;
+  //         isLoading = false;
+  //       });
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
+
   @override
   void initState() {
-    super.initState();    
-    getData();
+    super.initState();
+    // getData();
+    // getData();
+    getDataSikomo();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        // ignore: deprecated_member_use
-        child: 
-            ListView(
-          children: [
-            // display in the top screen
-            Stack(
-              children: [
-                // backorund image on top
-                Image.asset(
-                  'assets/images/air_terjun_homeScreen.png',
-                  fit: BoxFit.cover,
-                  height: 295,
-                  width: double.infinity,
-                ),
+          // ignore: deprecated_member_use
+          child: ListView(
+        children: [
+          // display in the top screen
+          Stack(
+            children: [
+              // backorund image on top
+              Image.asset(
+                'assets/images/air_terjun_homeScreen.png',
+                fit: BoxFit.cover,
+                height: 295,
+                width: double.infinity,
+              ),
 
-                // text selamat datang
-                Padding(
-                  padding: const EdgeInsets.only(top: 43, left: 13),
-                  child: Text(
-                    'Hai \nSelamat datang di SiKomo',
-                    style: Typograph.regulerMedium.copyWith(color: Colors.white),
-                    textAlign: TextAlign.left,
+              // text selamat datang
+              Padding(
+                padding: const EdgeInsets.only(top: 43, left: 13),
+                child: Text(
+                  'Hai \nSelamat datang di SiKomo',
+                  style: Typograph.regulerMedium.copyWith(color: Colors.white),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+
+              // text temukan keindahan pariwisata
+              Padding(
+                padding: const EdgeInsets.only(top: 116, left: 14),
+                child: Text(
+                  'Temukan Keindahan \nPariwisata Mojokerto',
+                  style: Typograph.semiBoldLarge.copyWith(color: Colors.white),
+                ),
+              ),
+
+              // textfiled untuk search
+              Container(
+                width: 265,
+                height: 51,
+                margin: const EdgeInsets.only(top: 265, left: 51),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(color: Colors.grey, blurRadius: 4)
+                  ],
+                ),
+                child: TextField(
+                  onChanged: ((value) => searchApi(value)),
+                  controller: search,
+                  textAlign: TextAlign.left,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'mau kemana?',
+                    hintStyle:
+                        Typograph.regulerMedium.copyWith(color: Colors.grey),
+                    border: InputBorder.none,
                   ),
                 ),
-
-                // text temukan keindahan pariwisata
-                Padding(
-                  padding: const EdgeInsets.only(top: 116, left: 14),
-                  child: Text(
-                    'Temukan Keindahan \nPariwisata Mojokerto',
-                    style: Typograph.semiBoldLarge.copyWith(color: Colors.white),
-                  ),
-                ),
-
-                // textfiled untuk search
-                Container(
-                  width: 265,
-                  height: 51,
-                  margin: const EdgeInsets.only(top: 265, left: 51),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: const [
-                      BoxShadow(color: Colors.grey, blurRadius: 4)
-                    ],
-                  ),
-                  child: TextField(
-                    onChanged: (value) {
-                      value = value.toLowerCase();
-                      setState(() {});
-                    },
-                    controller: search,
-                    textAlign: TextAlign.left,
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.search),
-                      hintText: 'mau kemana?',
-                      hintStyle:
-                          Typograph.regulerMedium.copyWith(color: Colors.grey),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            // spasi
-            const SizedBox(
-              height: 19,
-            ),
-
-            // tampilan product
-            isLoading
-                ? const Center(child: CircularProgressIndicator())
-                : SizedBox(
-                    height: MediaQuery.of(context).size.height / 1.8,
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      scrollDirection: Axis.vertical,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 11,
-                      padding: const EdgeInsets.all(5.0),
-                      children: List.generate(
-                        dataSikomo.length,
-                        (index) => ProductWisata(
-                          imageUrl: dataSikomo[index]['image'],
-                          name: dataSikomo[index]['nama'],
-                          rating: dataSikomo[index]['rating'],
-                          price: dataSikomo[index]['harga'],
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (ctx) => TourDetailsScreen(
-                                  imagebackground: dataSikomo[index]['image'],
-                                  title: dataSikomo[index]['nama'],
-                                  rating: dataSikomo[index]['rating'],
-                                  desciption: dataSikomo[index]['deskripsi'],
-                                  location: dataSikomo[index]['lokasi'],
-                                  jam: dataSikomo[index]['jam'],
-                                  price: dataSikomo[index]['harga'],
-                                  fasilitas_1: dataSikomo[index]['fasilitas_1'],
-                                  fasilitas_2: dataSikomo[index]['fasilitas_2'],
-                                ),
+              ),
+            ],
+          ),
+          // spasi
+          const SizedBox(
+            height: 19,
+          ),
+          // tampilan product
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : SizedBox(
+                  height: MediaQuery.of(context).size.height / 1.8,
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    scrollDirection: Axis.vertical,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 11,
+                    padding: const EdgeInsets.all(5.0),
+                    children: List.generate(
+                      displaySikomo.length,
+                      (index) => ProductWisata(
+                        imageUrl: displaySikomo[index].image,
+                        name: displaySikomo[index].nama,
+                        rating: displaySikomo[index].rating,
+                        price: displaySikomo[index].harga,
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (ctx) => TourDetailsScreen(
+                                imagebackground: displaySikomo[index].image,
+                                title: displaySikomo[index].nama,
+                                rating: displaySikomo[index].rating,
+                                desciption: displaySikomo[index].deskripsi,
+                                location: displaySikomo[index].lokasi,
+                                jam: displaySikomo[index].jam,
+                                price: displaySikomo[index].harga,
+                                fasilitas_1: displaySikomo[index].fasilitas_1,
+                                fasilitas_2: displaySikomo[index].fasilitas_2,
                               ),
-                            );
-                          },
-                        ),
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
-          ],
-        )
-      ),
+                ),
+        ],
+      )),
     );
   }
 }
